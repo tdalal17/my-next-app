c5b7d8ffccfb9bda3bbf1ad9f62f330cb0e4d1bc
Tanay Dalal <tdalal@hawk.iit.edu>
2023-11-23 20:18:06 -0600

    chang style code and add more clear to understand

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import Link from 'next/link';
 
-const HomePage = () => {
+const Home: React.FC = () => {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-4xl font-bold">Welcome to My Next.js App</h1>
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold mb-6">Welcome to My Next.js App</h1>
+      <p>This is the home page of the application.</p>
+      <nav className="flex flex-col mt-4 space-y-2">
+        <Link href="/about">
+          <a className="text-blue-600 hover:underline">About Us</a>
+        </Link>
+        <Link href="/api-data">
+          <a className="text-blue-600 hover:underline">API Data</a>
+        </Link>
+      </nav>
     </div>
   );
 };
-import Link from 'next/link';
 
-<Link href="/api-data">
-  <a className="text-blue-500 hover:text-blue-700">Go to API Data Page</a>
-</Link>
+export default Home;

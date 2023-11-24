02d60b7b5ff98cd43f8a3df9efb0a695bf4a98d4
Tanay Dalal <tdalal@hawk.iit.edu>
2023-11-23 20:54:25 -0600

    back to old one

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,8 @@ const Home: React.FC = () => {
       <h1 className="text-4xl font-bold mb-6">Welcome to My Next.js App</h1>
       <p>This is the home page of the application.</p>
       <nav className="flex flex-col mt-4 space-y-2">
-      <Link href="/about">
-          <a className="text-blue-600 hover:underline">About Us</a>
+        <Link href="/about">
+         <a className="text-blue-600 hover:underline">About Us</a>
         </Link>
         <Link href="/api-data">
           <a className="text-blue-600 hover:underline">API Data</a>

07d8d42414b681186efaca5fe85f27796f898007
Tanay Dalal <tdalal@hawk.iit.edu>
2023-11-23 20:07:15 -0600

    added link

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,16 @@
+import React from 'react';
+import Link from 'next/link'; 
+
 const About = () => {
-    return (
-      <div className="flex h-screen justify-center items-center">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold">About Us</h1>
-          <p className="mt-4">This is the about page for our Next.js app.</p>
-        </div>
-      </div>
-    );
-  };
-  
-  export default About;
-  
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold mb-6">About Us</h1>
+      <p>This is the about page for our Next.js app.</p>
+      <Link href="/">
+        <a className="text-blue-600 hover:underline">Go back home</a>
+      </Link>
+    </div>
+  );
+};
+
+export default About;

d8ace32a57d22f6183ed1a3609f5e586e98ee4c3
Tanay Dalal <tdalal@hawk.iit.edu>
2023-11-23 19:15:17 -0600

    made some changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-const Home = () => {
-    return (
-      <div>
-        <h1>Welcome to my Next.js application!</h1>
-      </div>
-    );
-  };
-  
-  export default Home;
-  
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold">Welcome to My Next.js App</h1>
+    </div>
+  );
+};
+
+export default HomePage;

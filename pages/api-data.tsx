0d1a0542d153b0e73d4301bf710a4ae4f23d7f2e
Tanay Dalal <tdalal@hawk.iit.edu>
2023-11-23 19:19:00 -0600

    ApiDataPage and setposts

---
+++ pages/api-data.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+const ApiDataPage = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data: Post[] = await response.json();
+      setPosts(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold">API Data</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            <h2 className="text-xl font-semibold">{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ApiDataPage;

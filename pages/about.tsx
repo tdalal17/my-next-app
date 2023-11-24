4d9a1672f718ce07f8caff4d559dcb1339f19bb4
Tanay Dalal <tdalal@hawk.iit.edu>
2023-11-23 20:01:02 -0600

    about us page

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+const About = () => {
+    return (
+      <div className="flex h-screen justify-center items-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold">About Us</h1>
+          <p className="mt-4">This is the about page for our Next.js app.</p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default About;
+  
